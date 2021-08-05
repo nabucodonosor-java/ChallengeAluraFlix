0f5ef95e254c51a6bff9d01704ee2cabf3449d36
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-08-05 11:21:42 -0300

    Deploy Heroku

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_user (name, email, password) VALUES ('User', 'user@mapin.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('User', 'user@mapin.com', '$2a$10$185gCqcjjheYPmdVcxfTCuhc2dL/i7uRF0kred0MpwCe1dD7twjsq');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_USER');
 
