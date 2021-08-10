901dff0e31f33a3a40feb5bc9928c07770b7f9ef
nabucodonosor-java <nabucodonosor.java@email.com>
2021-08-10 10:47:11 -0300

    Home layout 1200 and 1366px OK

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,27 +1,32 @@
-import MainImage from 'assets/images/main-image.svg';
+import MainImage from 'assets/images/main-image.jpg';
 import ButtonIcon from 'components/ButtonIcon';
 import Navbar from 'components/Navbar';
 import './styles.css';
 
 const Home = () => {
-
-    return (
-        <>
-        <Navbar />
-        <div className="home-container">
-            <div className="home-card">
-                <div className="home-content-container">
-                    <h1>React App Alura Flix</h1>
-                    <p>Interface gráfica desenvolvida em React + TS para consumir API desenvolvida durante o ChallengeAluraBackend</p>
-                    <ButtonIcon />
-                </div>
-                <div className="home-image-container">
-                    <img src={MainImage} alt="imagem"/>
-                </div>
+  return (
+    <>
+      <Navbar />
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <div>
+              <h1>React App Alura Flix</h1>
+              <p>
+                Interface gráfica desenvolvida em React + TS para consumir API
+                desenvolvida durante o desafio de backend da Alura.
+                <strong>#ChallengeAluraBackend</strong>
+              </p>
             </div>
+            <ButtonIcon />
+          </div>
+          <div className="home-image-container">
+            <img src={MainImage} alt="imagem" />
+          </div>
         </div>
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default Home;
