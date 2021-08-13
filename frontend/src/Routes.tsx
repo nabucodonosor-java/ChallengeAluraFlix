bdbf169f927ac55460d66662092025c111b9493a
nabucodonosor-java <nabucodonosor.java@email.com>
2021-08-13 18:54:21 -0300

    Starting dev /admin/auth

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,5 +1,6 @@
 import Navbar from "components/Navbar";
 import Admin from "pages/Admin";
+import Auth from "pages/Admin/Auth";
 import Home from "pages/Home";
 import VideoDetails from "pages/VideoDetails";
 import VideoList from "pages/VideoList";
@@ -17,6 +18,9 @@ const Routes = () => (
             </Route>
             <Route path="/videos/:videoId">
                 <VideoDetails />
+            </Route>
+            <Route path="/admin/auth">
+                <Auth />
             </Route>
             <Redirect from="/admin" to="/admin/videos" exact />
             <Route path="/admin">
--- frontend/src/pages/Admin/Auth/index.tsx
+++ frontend/src/pages/Admin/Auth/index.tsx
@@ -1,9 +1,31 @@
+import AuthImg from 'assets/images/auth-image.png';
+import { Route, Switch } from 'react-router-dom';
+
 import './styles.css';
 
 const Auth = () => {
   return ( 
-    <div>
-      
+    <div className="auth-container">
+      <div className="auth-banner-container">
+        <h1>Alura Flix</h1>
+        <p>Área de autenticação</p>
+        <img src={AuthImg} alt="logo"/>
+      </div>
+
+      <div className="auth-form-container base-card">
+        <Switch>
+          <Route path="/admin/auth/login">
+            <h1>Card de Login</h1>
+          </Route>
+          <Route path="/admin/auth/cadastro">
+            <h1>Card de Cadastro</h1>
+          </Route>
+          <Route path="/admin/auth/recuperar">
+            <h1>Card de Recuperação de senha</h1>
+          </Route>
+        </Switch>
+      </div>
+       
     </div>
   );
 }
